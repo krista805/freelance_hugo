344e323cd36d01caca086f8b7bbb78db92489871
Krista Simmons <kristasimmons.design@gmail.com>
2018-06-10 18:25:40 -0700

    Append / to javascript script tag

--- static/js/functions.js
+++ static/js/functions.js
@@ -46,7 +46,7 @@ function workBelt() {
 
 function  workLoad() {
   
-  $.ajaxSetup({ cache: true });
+  $.ajaxSetup({ cache: false });
   
   $('.thumb-container label').click(function() {
     var $this = $(this),
